c12d58694a6b3ca188431e2a555f94e2ac19fac8
redswallow <redswallowjysc@gmail.com>
2012-09-28 23:00:00 +0800

    first commit

---
+++ douban-aiguo/aiguo.js
@@ -0,0 +1,56 @@
+﻿// ==UserScript==
+// @name          a new navigation of Douban
+// @author        redswallow
+// @include       http://www.douban.com/*
+// @match         http://www.douban.com/*
+// @include       http://book.douban.com/*
+// @match         http://book.douban.com/*
+// @include       http://music.douban.com/*
+// @match         http://music.douban.com/*
+// @include       http://movie.douban.com/*
+// @match         http://movie.douban.com/*
+// @version       0.1
+// ==/UserScript==
+
+function main($) {
+	function delete_ad(){
+		$('.db-ad').css("display","none");
+		$('#dale_group_topic_new_top_right').css("display","none");
+		$('#dale_group_home_middle_right').css("display","none");
+	};
+
+	function loved(){
+		$('.btn-like').text("爱过");
+		$('.btn-fav').text("爱过");
+		$('.like').text("爱过");
+		$('.btn-action-reply').text("來一發");
+	}
+	'use strict';
+	jQuery.noConflict();
+	loved();
+	delete_ad();
+}
+
+!
+function loader(i) {
+	var script, requires = ['http://img3.douban.com/js/packed_jquery.min6301986802.js'],
+	    head = document.getElementsByTagName('head')[0],
+	    makeScript = function () {
+		    script = document.createElement('script');
+		    script.type = 'text/javascript';
+	    },
+	    loadLocal = function (fn) {
+		    makeScript();
+		    script.textContent = '(' + fn.toString() + ')(jQuery);';
+		    head.appendChild(script);
+	    };
+	(function (i) {
+		makeScript();
+		script.src = requires[i];
+		script.addEventListener('load', function () {
+			++i !== requires.length ? loader(i):loadLocal(main);
+		}, true);
+		head.appendChild(script);
+	})(i || 0);
+}();
+
