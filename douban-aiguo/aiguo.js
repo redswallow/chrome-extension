769a38dc9088b6b70dc3274bd4b98f16e913a374
redswallow <redswallowjysc@gmail.com>
2012-09-29 16:52:55 +0800

    fix loved function bug

--- douban-aiguo/aiguo.js
+++ douban-aiguo/aiguo.js
@@ -23,7 +23,17 @@ function main($) {
 		$('.btn-like').text("爱过");
 		$('.btn-fav').text("爱过");
 		$('.like').text("爱过");
-		$('.btn-action-reply').text("來一發");
+		$('.btn-reshare').text("喜闻乐见");
+		$('.btn-action-reply').text(function() {
+			var numlen=$(this).text().indexOf("回应")
+			if(numlen==0){
+				return "来一发";
+			}
+			else{
+				return $(this).text().substring(0,numlen)+"发";
+			}
+		});
+		
 	}
 
 	function hover(){
