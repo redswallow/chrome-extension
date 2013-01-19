336bdac138ede266dcf5a1cbec920d8abfd9a6d0
redswallow <redswallowjysc@gmail.com>
2013-01-19 12:53:34 +0800

    update dmm extension

--- dmm/dmm.js
+++ dmm/dmm.js
@@ -4,34 +4,10 @@
 // @version       0.1
 // ==/UserScript==
 
-function main($) {
-	'use strict';
-	jQuery.noConflict();
+load=function () {
 	if($('.pd-3>a').length){
 		$('.tdmm').replaceWith("<img src='"+$('.pd-3>a').attr("href")+"' />")
 	}
 }
 
-!
-function loader(i) {
-	var script, requires = ['http://img3.douban.com/js/packed_jquery.min6301986802.js'],
-	    head = document.getElementsByTagName('head')[0],
-	    makeScript = function () {
-		    script = document.createElement('script');
-		    script.type = 'text/javascript';
-	    },
-	    loadLocal = function (fn) {
-		    makeScript();
-		    script.textContent = '(' + fn.toString() + ')(jQuery);';
-		    head.appendChild(script);
-	    };
-	(function (i) {
-		makeScript();
-		script.src = requires[i];
-		script.addEventListener('load', function () {
-			++i !== requires.length ? loader(i):loadLocal(main);
-		}, true);
-		head.appendChild(script);
-	})(i || 0);
-}();
-
+load()
